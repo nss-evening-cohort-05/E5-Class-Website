4d9bf6cb4e5040480d6c1ec526fe4f42c85aee0b
webbdm <gwebb762@gmail.com>
2017-12-16 11:35:05 -0600

    Added Bulma and Card grid

--- js/main.js
+++ js/main.js
@@ -7,49 +7,36 @@ const getStudents = async (url) => {
     }
 };
 
-// Populate variables 
-let studentCard = ` 
-`;
+let studentBox = document.getElementById('studentBox');
 
 let printStudent = (student) => {
-    studentBox += `<div class="card">
+    studentBox.innerHTML += `<div class="card student-card">
     <div class="card-image">
         <figure class="image is-4by3">
             <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
         </figure>
     </div>
-    ${console.log("Hi")}
     <div class="card-content">
         <div class="media">
-            <div class="media-left">
-                <figure class="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                </figure>
-            </div>
             <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
+                <p class="title is-4">${student.name}</p>
+                <p class="subtitle is-6">@${student.github}</p>
             </div>
         </div>
 
         <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-            <a>@bulmaio</a>.
-            <a href="#">#css</a>
-            <a href="#">#responsive</a>
+            <p>${student.bio}</p>
             <br>
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
     </div>
 </div>`;
 };
 
-let printStudents = getStudents('students.json').then((results)=>{console.log(results)});
+let printStudents = () => getStudents('students.json').then((results) => {
+    results.students.map((student) => {
+        printStudent(student);
+    });
+});
+printStudents();
 
-console.log(printStudents);
 
-//printStudents.map(student => studentBox += )
-
-let studentBox = document.getElementById('studentBox');
-
-//studentBox.innerHTML += getStudents('students.json');
