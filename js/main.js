55dbf0ba12456d462c0db54eb3ff1df4608f17d6
webbdm <gwebb762@gmail.com>
2017-12-17 21:29:59 -0600

    Added nav & class JSON

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ let studentBox = document.getElementById('studentBox');
 let printStudent = (student) => {
     studentBox.innerHTML += `<div class="card student-card">
     <div class="card-image">
-        <figure class="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        <figure class="image is-4by4">
+            <img src="${student.photo}" alt="Placeholder image">
         </figure>
     </div>
     <div class="card-content">
